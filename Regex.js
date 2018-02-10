77e3091fa04d5dcf909831383baa3347bd31e8ba
HOWZ1T <dylan.d.randall@gmail.com>
2018-02-10 21:36:51 +1100

    Update Regex.js

--- Regex.js
+++ Regex.js
@@ -1,5 +1,6 @@
 //REGEX FOR MATCHING EMAILS BASED ON THE EMAIL STRUCTURE DETAILED AT: https://en.wikipedia.org/wiki/Email_address
 let emailRegex = /^[^.][a-zA-Z0-9!#$%&'*+-/=?^_`{|}~.]{1,64}@{1}[a-zA-Z0-9-.]{1,255}.[a-zA-Z0-9-.]{1,63}\.[a-zA-Z0-9]{1,63}$/;
+let emailRegexVariant /^[^.][a-zA-Z0-9!#$%&'*+-\/=?^_`{|}~.]{1,64}@[a-zA-Z0-9-.]{1,255}\.[a-zA-Z0-9-.]{1,63}[^. ]$/; //slightly improved email matching regex
 let nameRegex = /^[a-zA-Z .,'-]+$/;
 let addressRegex = /^[a-zA-Z0-9 .,'-/\n]+$/;
 let phoneRegex = /^(\+614|[(]04[)]|04)[ ][0-9]{3,3}[ ][0-9]{3,3}[ ][0-9]{2,2}$/; //FOR AUSTRALIAN PHONE NO.S
