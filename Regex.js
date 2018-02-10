f6de88267d172e21399c72d4f135b8330eaae9b5
HOWZ1T <dylan.d.randall@gmail.com>
2018-02-10 20:28:09 +1100

    Create Regex.js

---
+++ Regex.js
@@ -0,0 +1,8 @@
+//REGEX FOR MATCHING EMAILS BASED ON THE EMAIL STRUCTURE DETAILED AT: https://en.wikipedia.org/wiki/Email_address
+let emailRegex = /^[^.][a-zA-Z0-9!#$%&'*+-/=?^_`{|}~.]{1,64}@{1}[a-zA-Z0-9-.]{1,255}.[a-zA-Z0-9-.]{1,63}\.[a-zA-Z0-9]{1,63}$/;
+let nameRegex = /^[a-zA-Z .,'-]+$/;
+let addressRegex = /^[a-zA-Z0-9 .,'-/\n]+$/;
+let phoneRegex = /^(\+614|[(]04[)]|04)[ ][0-9]{3,3}[ ][0-9]{3,3}[ ][0-9]{2,2}$/; //FOR AUSTRALIAN PHONE NO.S
+let creditCardRegex = /^[^ ][ 0-9]{12,19}$/;
+let visaRegex = /^4[ 0-9]{13,18}$/;
+let masterCardRegex = /^5[0-5]([ 0-9]){12,21}$/;
